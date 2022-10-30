479c83c943fafb9eb17cb2eb0c086586a5a4f49f
Camiari <camiari.zamd@gmail.com>
2022-10-29 21:26:22 -0500

    CRUD tabla artista realizado en controlador y router

--- proyecto_back/controllers/artistas.controller.js
+++ proyecto_back/controllers/artistas.controller.js
@@ -31,3 +31,60 @@ exports.create = function(req,res){ //creo un nuevo artista
         res.json(response) //Se corrige el error de sintaxis, ya que no es reponse.jason sino res.jason
     })
 }
+
+// Función listar artistas de la base de datos:
+exports.find = function(req, res){
+    Artista.find(function(err, artistas){
+        res.json(artistas)
+    })
+}
+
+// Función encontrar un artista por id:
+exports.findOne = function(req, res){
+    Artista.findOne({_id: req.params.id}, function(err, artista){
+        res.json(artista)
+    })
+}
+
+// Función modificar un artista por el id:
+exports.update = function(req, res){
+    let artista = {
+        nombre: req.body.nombre,
+        apellido_p: req.body.apellido_p,
+        apellido_m: req.body. apellido_m,
+        telefono: req.body.telefono,
+        mail: req.body.mail,
+        direccion: req.body.direccion
+    }
+
+    Artista.findByIdAndUpdate(req.params.id, {$set: artista}, function(err){
+        if(err){
+            console.error(err),
+            response.exito = false,
+            response.msg = "Error al modificar el artista"
+            res.json(response)
+            return;
+        }
+
+        response.exito = true,
+        response.msg = "El artista fue modificado correctamente"
+        res.json(response)
+    })
+}
+
+// Función eliminar artista:
+exports.remove = function(req, res){
+    Artista.findByIdAndRemove({_id: req.params.id}, function(err){
+        if(err){
+            console.error(err)
+            response.exito = false,
+            response.msg = "Error al eliminar el artista"
+            res.json(response)
+            return;
+        }
+
+        response.exito = true,
+        response.msg = "El artista ha sido eliminado correctamente"
+        res.json(response)
+    })
+}
--- proyecto_back/routes/artistas.router.js
+++ proyecto_back/routes/artistas.router.js
@@ -2,6 +2,19 @@ const express = require("express")
 const router = express.Router()
 const artistasController = require("../controllers/artistas.controller")
 
+//Crear un artista:
 router.post("/", artistasController.create)
+
+//Listar todos los artistas:
+router.get("/", artistasController.find)
+
+//Encontrar por id un artista:
+router.get("/:id", artistasController.findOne)
+
+//Modificar un artista:
+router.put("/:id", artistasController.update)
+
+//Eliminar un artista:
+router.delete("/:id", artistasController.remove)
 
 module.exports = router
